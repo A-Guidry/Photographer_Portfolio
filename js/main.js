5deac842de257e8c1a37e9293f3fdd8af746f21d
AGuidry <anthonyguidry1@me.com>
2016-08-22 07:45:30 -0700

    Minor Issues to be fixed

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// $(document).ready(function() {
+// 	$('.topNav').mouseover(function() {
+// 		$("#navTop").addClass('animated infinate pulse');
+// 		$("#navTop").addClass('animated pulse');
+// 		$("#navTop").addClass('animated pulse');
+// 		$("#navTop").addClass({'animated pulse'} 300);
+		
+// 	})
+
+
+
+
+
+
+
+
+
+
+
+
+
+// }) //End of doc ready
