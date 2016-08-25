9bacc9abb1daa0d4f29b0c8e53d3153aebc4639d
AGuidry <anthonyguidry1@me.com>
2016-08-24 21:28:09 -0700

    Updatedd nav bar, added porftolio hover and fixed contact section issue.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
 // $(document).ready(function() {
-// 	$('.topNav').mouseover(function() {
-// 		$("#navTop").addClass('animated infinate pulse');
-// 		$("#navTop").addClass('animated pulse');
-// 		$("#navTop").addClass('animated pulse');
-// 		$("#navTop").addClass({'animated pulse'} 300);
-		
-// 	})
-
-
-
-
-
-
-
-
-
-
-
 
 
 // }) //End of doc ready
